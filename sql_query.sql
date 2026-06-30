9fbe13bafb162adc98886693c415a3ad1b591bb2
smmahmudulhasansiddiquei <mahmudcse03@gmail.com>
2026-06-30 23:03:57 +0600

    add more queries

--- sql_query.sql
+++ sql_query.sql
@@ -20,19 +20,97 @@ FROM customers
 WHERE city='Dhaka';
 
 --Products below 2000
-
 SELECT *
 FROM products
 WHERE price<2000;
 
 --Delivered Orders
-
 SELECT *
 FROM orders
 WHERE order_status='Delivered';
 
 --Pending Payments
-
 SELECT *
 FROM payments
 WHERE payment_status='Pending';
+
+
+-- ORDER BY ---
+--Products by Highest Price
+SELECT *
+FROM products
+ORDER BY price DESC;
+
+--Cheapest Products
+SELECT *
+FROM products
+ORDER BY price ASC;
+
+-- LIMIT --
+--Top 5 Expensive Products
+SELECT *
+FROM products
+ORDER BY price DESC
+LIMIT 5;
+
+--LIKE
+--Customer names starting with M
+SELECT *
+FROM customers
+WHERE full_name LIKE 'M%';
+
+--Email ending gmail.com
+SELECT *
+FROM customers
+WHERE email LIKE '%gmail.com';
+
+--betweeen
+--Products between 1000 and 5000
+SELECT *
+FROM products
+WHERE price BETWEEN 1000 AND 5000;
+
+--Salary between 35000 and 50000
+SELECT *
+FROM employees
+WHERE salary BETWEEN 35000 AND 50000;
+
+--Aggregate Functions
+--Total Customers
+SELECT COUNT(*) AS Total_Customers
+FROM customers;
+
+--Total Products
+SELECT COUNT(*) AS Total_Products
+FROM products;
+
+--Average Product Price
+SELECT AVG(price) AS Average_Price
+FROM products;
+
+--Total Stock
+SELECT SUM(stock_quantity) AS Total_Stock
+FROM products;
+
+--Average Employee Salary
+SELECT AVG(salary) AS Average_Salary
+FROM employees;
+
+--AND,OR--
+--Electronics products above 50000
+SELECT *
+FROM products
+WHERE category_id=1
+AND price>50000;
+
+--Products below 5000 OR stock above 50
+SELECT *
+FROM products
+WHERE price<5000
+OR stock_quantity>50;
+
+--Customers from Dhaka having Gmail
+SELECT *
+FROM customers
+WHERE city='Dhaka'
+AND email LIKE '%gmail.com';
